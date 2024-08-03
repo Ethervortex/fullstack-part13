5b595a3416661c1d8b55bf5317b4238ec2c34914
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-08-03 13:34:31 +0300

    Ex13.6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
 router.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     console.log(JSON.stringify(blogs, null, 2))
@@ -18,12 +23,40 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.delete('/:id', async (req, res) => {
+router.get('/:id', blogFinder, async (req, res) => {
   try {
-    const blog = await Blog.findByPk(req.params.id)
-    if (blog) {
-      console.log(JSON.stringify(blog))
-      await blog.destroy()
+    if (req.blog) {
+      res.json(req.blog)
+    } else {
+      res.status(404).json({ error: 'Blog not found' })
+    }
+  } catch (error) {
+    console.error('Error fetching blog:', error)
+    res.status(500).json({ error: 'Error fetching blog' })
+  }
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  try {
+    if (req.blog) {
+      const { likes } = req.body
+      req.blog.likes = likes
+      await req.blog.save()
+      res.json(req.blog)
+    } else {
+      res.status(404).json({ error: 'Blog not found' })
+    }
+  } catch (error) {
+    console.error('Error increasing likes:', error)
+    res.status(500).json({ error: 'Error increasing likes' })
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  try {
+    if (req.blog) {
+      console.log(JSON.stringify(req.blog))
+      await req.blog.destroy()
       res.status(204).end()
     } else {
       res.status(404).end()
