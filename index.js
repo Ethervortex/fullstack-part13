07631d901f59c7e20320d29458c61efd0c1efe71
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-08-03 17:57:08 +0300

    Ex13.9

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.use('/api/login', loginRouter)
 
 const errorHandler = (error, request, response, next) => {
   console.error('Error:', error)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'SequelizeUniqueConstraintError') {
+    return response.status(400).json({ error: 'username must be unique' })
+  } else if (error.name === 'SequelizeValidationError') {
+    return response.status(400).json({
+      error: error.errors.map(e => e.message)
+    })
+  }
   response.status(error.status || 500).json({
     error: error.message || 'Internal Server Error'
   })
