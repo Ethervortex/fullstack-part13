d7ab94414118926a1ccf63ce657ddffad841d9b5
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-08-05 12:37:23 +0300

    Ex13.10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
+const { SECRET } = require('../util/config')
+const jwt = require('jsonwebtoken')
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
@@ -10,15 +12,42 @@ const blogFinder = async (req, res, next) => {
   next()
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  console.log('authorization:', authorization.substring(7))
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+      console.log('Decoded token:', req.decodedToken)
+    } catch{
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  //const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User,
+      attributes: ['name']
+    }
+  })
   console.log(JSON.stringify(blogs, null, 2))
   res.json(blogs)
 })
   
-router.post('/', async (req, res) => {
+router.post('/', tokenExtractor, async (req, res) => {
   console.log(req.body)
-  const blog = await Blog.create(req.body)
+  //const blog = await Blog.create(req.body)
+  //const user = await User.findOne()
+  //const blog = await Blog.create({...req.body, userId: user.id})
+  const user = await User.findByPk(req.decodedToken.id)
+  const blog = await Blog.create({...req.body, userId: user.id, date: new Date()})
   res.json(blog)
 })
 
