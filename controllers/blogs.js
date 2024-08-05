d20322c7f1166b820f1650d7aaa69a086aad724a
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-08-05 16:57:41 +0300

    Ex13.12

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -29,7 +29,6 @@ const tokenExtractor = (req, res, next) => {
 
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll({
-    attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,10 @@ const router = require('express').Router()
 const { User, Blog } = require('../models')
 
 router.get('/', async (req, res) => {
-  //const users = await User.findAll()
   const users = await User.findAll({
     include: {
       model: Blog,
-      attributes: { exclude: ['userId'] }
+      attributes: ['title', 'url', 'author', 'likes']
     }
   })
   res.json(users)
